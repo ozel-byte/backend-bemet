11dece96fc4edb23bf891df4d8733537aba39d4e
ozel-byte <ozelcode@gmail.com>
2023-02-13 17:26:30 -0600

    Nueva version

--- firebase.js
+++ firebase.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const {initializeApp, applicationDefault} = require('firebase-admin/app');
 const {getFirestore} = require('firebase-admin/firestore')
+
 initializeApp({
-    credential: applicationDefault()
+    credential: applicationDefault(),
 })
 const db = getFirestore()
+
 module.exports = {
     db
 }
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const { db } = require('./firebase');
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const { response } = require("express");
+const upload = multer({ dest: 'uploads/' });
+
+
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+cloudinary.config({
+    cloud_name: 'dv5fwf13g',
+    api_key: '296484175841721',
+    api_secret: 'DJIqJ_Wi_Qg4jgSKlNVvZGPnQqU',
+    secure: true
+})
 
 //finish login
 app.get('/login', async (req, res) => {
-    console.log(req.query);
+
     const username = req.query.username;
     const password = req.query.password;
     const querySnapshot = await db.collection('User').get();
@@ -14,23 +28,42 @@ app.get('/login', async (req, res) => {
 
 })
 
+app.post('/create-account', async (req, res) => {
+    const nombre = req.body.name;
+    const password = req.body.password;
+    const rol = req.body.rol;
+
+    const querySnapshot = await db.collection('User').add({
+        "user-name": nombre,
+        password,
+        rol
+    });
+    res.json({
+        "status": "true"
+    });
+})
+
 validarUser = (docs, username, password) => {
     for (const user of docs) {
         if (user.data()["user-name"] === username && user.data()["password"] === password) {
             return {
                 "status": "true",
-                "message": "authorization"
-            };
-        } else {
-            return {
-                "status": "false",
-                "message": "No existen esas credenciales"
+                "message": "authorization",
+                "rol": user.data()["rol"]
             };
         }
     }
+    return {
+        "status": "false",
+        "message": "No existen esas credenciales"
+    };
 }
 
-app.post('/add-equipo-medico', async (req, res) => {
+app.post('/add-equipo-medico', upload.array("img"), async (req, res) => {
+  const response = await cloudinary.uploader.upload(
+    req.files[0]["path"],
+  )
+  console.log(response.url);
     const nombre = req.body.nombre;
     const marca = req.body.marca;
     const piezas = req.body.piezas;
@@ -39,17 +72,17 @@ app.post('/add-equipo-medico', async (req, res) => {
         "name": nombre,
         marca,
         piezas,
-        referencia
+        referencia,
+        "img": response.url
     });
     res.send({
         "status": "true",
-        "message": "chidoo"
+        "message": "correct"
     })
 })
 
 app.get('/equipo-medico', async (req, res) => {
     const querySnapshot = await db.collection('Medicos').get();
-    console.log(querySnapshot.docs[0].data());
     let photoList = [];
     for (const photo of querySnapshot.docs) {
         photoList.push(photo.data());
@@ -59,7 +92,7 @@ app.get('/equipo-medico', async (req, res) => {
     });
 })
 
-app.post('/add-consumibles', async (req, res) => {
+app.post('/add-consumibles', upload.array("img"), async (req, res) => {
     const nombre = req.body.nombre;
     const marca = req.body.marca;
     const piezas = req.body.piezas;
@@ -67,20 +100,24 @@ app.post('/add-consumibles', async (req, res) => {
     const ubicacion = req.body.ubicacion;
     const descripcion = req.body.descripcion;
     const compatible = req.body.compatible;
+    const response = await cloudinary.uploader.upload(
+        req.files[0]["path"],
+      );
 
     if (nombre != "" && marca != "" && piezas != "") {
-        const response = await db.collection("Consumibles").add({
+        const res = await db.collection("Consumibles").add({
             "name": nombre,
             marca,
             piezas,
             ubicacion,
             "referencia": ref,
             descripcion,
-            "compatible-con": compatible
+            "compatible-con": compatible,
+            "img": response.url
         });
-        console.log(response);
+        console.log(res);
         res.send({
-            "status": "si se pudo crear"
+            "status": "true"
         })
     } else {
         res.send({
@@ -92,7 +129,6 @@ app.post('/add-consumibles', async (req, res) => {
 
 app.get('/consumibles', async (req, res) => {
     const querySnapshot = await db.collection('Consumibles').get();
-    console.log(querySnapshot.docs[0].data());
     let photoList = [];
     for (const photo of querySnapshot.docs) {
         photoList.push(photo.data());
@@ -115,6 +151,34 @@ app.get('/search-consumible', async (req, res) => {
     res.json({
         "data": consumibles
     });
+})
+app.get('/search-medical', async (req, res) => {
+    const query = req.query.query;
+
+    if (query == "") {
+        res.json({
+            "data": []
+        })
+    } else {
+        const querySnapshot = await db.collection('Medicos').get();
+        let medicos = [];
+        for (const medico of querySnapshot.docs) {
+            const nombre = medico.get("name");
+            if (nombre.toLowerCase().startsWith(query.toLowerCase())) {
+                medicos.push({
+                    "img": medico.get("img"),
+                    "marca": medico.get("marca"),
+                    "name": medico.get("name"),
+                    "piezas": medico.get("piezas"),
+                    "referencia": medico.get("referencia")
+                });
+            }
+        }
+        res.json({
+            "data": medicos
+        });
+
+    }
 })
 
 app.get('/', async (req, res) => {
